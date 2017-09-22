0f120608d6b23ded54a5c5041bf34141cf8592a5
Stephan Müller <mail@stephanmueller.eu>
2017-09-22 19:10:02 +0200

    update to new schema registry version

--- src/main/java/com/github/smueller18/flink/serialization/AvroBuilderKeyedSerializationSchema.java
+++ src/main/java/com/github/smueller18/flink/serialization/AvroBuilderKeyedSerializationSchema.java
@@ -1,55 +1,80 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017 Stephan Müller
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+
 package com.github.smueller18.flink.serialization;
 
+import java.util.Map;
+
 import com.github.smueller18.avro.builder.AvroBuilder;
-import kafka.utils.VerifiableProperties;
 import org.apache.flink.streaming.util.serialization.KeyedSerializationSchema;
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
 
-import java.util.Properties;
 
-/**
- * Copyright 2017 Stephan Müller
- * License: MIT
- */
 public class AvroBuilderKeyedSerializationSchema<T extends AvroBuilder> implements KeyedSerializationSchema<T> {
 
-    private Properties props;
+    private Map<String, ?> configs;
 
-    private transient KafkaAvroKeyEncoder keyEncoder;
-    private transient KafkaAvroValueEncoder valueEncoder;
+    private transient KafkaAvroSerializer keySerializer;
+    private transient KafkaAvroSerializer valueSerializer;
 
     /***
      *
-     * @param props properties for {@link KafkaAvroKeyEncoder} and {@link KafkaAvroValueEncoder}
+     * @param configs key-value-pairs for {@link KafkaAvroSerializer}
      *              schema.registry.url ({@link String}):
      *                  Comma-separated list of URLs for schema registry instances that can be used to register or look up schemas
      *              max.schemas.per.subject ({@link Integer}, default: 1000):
      *                  Maximum number of schemas to create or cache locally
      *
      */
-    public AvroBuilderKeyedSerializationSchema(Properties props) {
-        this.props = props;
+    public AvroBuilderKeyedSerializationSchema(Map<String, ?> configs) {
+        this.configs = configs;
     }
 
     @Override
-    public byte[] serializeKey(T avroBuilder) {
+    public byte[] serializeKey(T avroBuilderObject) {
 
-        if (this.keyEncoder == null)
-            this.keyEncoder = new KafkaAvroKeyEncoder(new VerifiableProperties(this.props));
+        if (this.valueSerializer == null) {
+            this.valueSerializer = new KafkaAvroSerializer();
+            this.valueSerializer.configure(this.configs, true);
+        }
 
-        return this.keyEncoder.toBytes(avroBuilder.getKeyRecord());
+        return this.keySerializer.serialize(AvroBuilder.getTopicName(avroBuilderObject.getClass()), avroBuilderObject.getKeyRecord());
     }
 
     @Override
-    public byte[] serializeValue(T avroBuilder) {
+    public byte[] serializeValue(T avroBuilderObject) {
 
-        if (this.valueEncoder == null)
-            this.valueEncoder = new KafkaAvroValueEncoder(new VerifiableProperties(this.props));
-
-        return this.valueEncoder.toBytes(avroBuilder.getValueRecord());
+        if (this.keySerializer == null) {
+            this.keySerializer = new KafkaAvroSerializer();
+            this.keySerializer.configure(this.configs, false);
+        }
+        return this.valueSerializer.serialize(AvroBuilder.getTopicName(avroBuilderObject.getClass()), avroBuilderObject.getValueRecord());
     }
 
     @Override
-    public String getTargetTopic(T avroBuilder) {
+    public String getTargetTopic(T avroBuilderObject) {
         return null;
     }
 
--- src/main/java/com/github/smueller18/flink/serialization/SchemaRegistryKeyedDeserializationSchema.java
+++ src/main/java/com/github/smueller18/flink/serialization/SchemaRegistryKeyedDeserializationSchema.java
@@ -1,58 +1,85 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017 Stephan Müller
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+
 package com.github.smueller18.flink.serialization;
 
-import io.confluent.kafka.serializers.KafkaAvroDecoder;
-import kafka.utils.VerifiableProperties;
+import io.confluent.kafka.serializers.KafkaAvroDeserializer;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.flink.api.java.typeutils.TypeExtractor;
 import org.apache.flink.streaming.util.serialization.KeyedDeserializationSchema;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
-import org.apache.kafka.common.errors.SerializationException;
 
 import java.io.IOException;
 import java.net.ConnectException;
-import java.util.Properties;
+import java.util.Map;
 
-/**
- * Copyright 2017 Stephan Müller
- * License: MIT
- */
 
 public class SchemaRegistryKeyedDeserializationSchema implements KeyedDeserializationSchema<GenericKeyValueRecord> {
 
-    private Properties props;
-    private transient KafkaAvroDecoder decoder;
+    private Map<String, ?> configs;
+    private transient KafkaAvroDeserializer keyDeserializer;
+    private transient KafkaAvroDeserializer valueDeserializer;
 
     /***
      *
-     * @param props properties for {@link KafkaAvroDecoder}
+     * @param configs key-value-pairs for {@link KafkaAvroDeserializer}
      *              schema.registry.url ({@link String}):
      *                  Comma-separated list of URLs for schema registry instances that can be used to register or look up schemas
      *              max.schemas.per.subject ({@link Integer}, default: 1000):
      *                  Maximum number of schemas to create or cache locally
      *
      */
-    public SchemaRegistryKeyedDeserializationSchema(Properties props) {
-        this.props = props;
+    public SchemaRegistryKeyedDeserializationSchema(Map<String, ?> configs) {
+        this.configs = configs;
     }
 
     @Override
     public GenericKeyValueRecord deserialize(byte[] messageKey, byte[] message, String topic, int partition, long offset)
             throws IOException {
 
-        if(decoder == null)
-            decoder = new KafkaAvroDecoder(new VerifiableProperties(this.props));
+        if(this.keyDeserializer == null) {
+            this.keyDeserializer = new KafkaAvroDeserializer();
+            this.keyDeserializer.configure(this.configs, true);
+        }
+
+        if(this.valueDeserializer == null) {
+            this.valueDeserializer = new KafkaAvroDeserializer();
+            this.valueDeserializer.configure(this.configs, false);
+        }
 
         try {
             return new GenericKeyValueRecord(
-                    (GenericRecord) decoder.fromBytes(messageKey),
-                    (GenericRecord) decoder.fromBytes(message)
+                    (GenericRecord) keyDeserializer.deserialize(null, messageKey),
+                    (GenericRecord) valueDeserializer.deserialize(null, message)
             );
         }
         catch(Exception e) {
             if (e.getCause() instanceof ConnectException)
                 throw new ConnectException(
                         String.format("Connection to schema registry '%s' could not be established.",
-                                this.props.getProperty("schema.registry.url")
+                                this.configs.get("schema.registry.url")
                         )
                 );
             else throw e;
